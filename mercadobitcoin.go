759984bcc8e435814fce8383aca65f230ba8a9e9
Arthur <awmpietro@hotmail.com>
2017-08-13 01:34:43 -0300

    First Commit

---
+++ mercadobitcoin.go
@@ -0,0 +1,49 @@
+package mcdbitcoindd
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var reqUrl = "https://www.mercadobitcoin.net/api/"
+
+func GetTicker() (*Ticker, error) {
+	resp, err := http.Get(reqUrl + "ticker")
+	if err != nil {
+		return nil, err
+	}
+	ticker := new(Ticker)
+	err = json.NewDecoder(resp.Body).Decode(&ticker)
+	if err != nil {
+		return nil, err
+	}
+	return ticker, nil
+}
+
+func GetOrderBook() (*Orderbook, error) {
+	resp, err := http.Get(reqUrl + "orderbook")
+	if err != nil {
+		return nil, err
+	}
+	orderBook := new(Orderbook)
+	err = json.NewDecoder(resp.Body).Decode(&orderBook)
+	if err != nil {
+		return nil, err
+	}
+	return orderBook, nil
+}
+
+func GetTrades() ([]TradesData, error) {
+	resp, err := http.Get(reqUrl + "trades")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	trades := make([]TradesData, 0)
+	err = json.Unmarshal(body, &trades)
+	if err != nil {
+		return nil, err
+	}
+	return trades, nil
+}
---
+++ model.go
@@ -0,0 +1,31 @@
+package mcdbitcoindd
+
+type TickerData struct {
+	High float32 `json:"high"`
+	Low  float32 `json:"low"`
+	Vol  float32 `json:"vol"`
+	Last float32 `json:"last"`
+	Buy  float32 `json:"buy"`
+	Sell float32 `json:"sell"`
+	Date int     `json:"date"`
+}
+type Ticker struct {
+	Ticker TickerData `json:"ticker"`
+}
+
+type Orderbook struct {
+	Asks [][]float32 `json:"asks"`
+	Bids [][]float32 `json:"bids"`
+}
+
+type TradesData struct {
+	Date   int     `json:"date"`
+	Price  float32 `json:"price"`
+	Amount float32 `json:"amount"`
+	Tid    int     `json:"tid"`
+	Type   string  `json:"type"`
+}
+
+type Trades struct {
+	Collection []TradesData
+}
